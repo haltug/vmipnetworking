64645a446c4d34fefba9274eae966462769b471e
Halis Altug <mail@halisaltug.de>
2015-08-15 00:59:03 +0200

    traci in inet adapted

--- inet/src/veins/modules/mobility/traci/TraCIScenarioManagerInet.h
+++
@@ -1,2 +0,0 @@
-void ifInetTraCIMobilityCallPreInitialize(cModule* mod, const std::string& nodeId, const inet::Coord& position, const std::string& road_id, double speed, double angle);
-void ifInetTraCIMobilityCallNextPosition(cModule* mod, const inet::Coord& p, const std::string& edge, double speed, double angle);
